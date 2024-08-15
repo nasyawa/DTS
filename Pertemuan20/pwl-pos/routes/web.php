e1796d784a7f181cb6e25076d566417580c24439
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2024-08-15 18:21:45 +0700

    Pertemuan 23

--- Pertemuan20/pwl-pos/routes/web.php
+++ Pertemuan20/pwl-pos/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\WelcomeController;
 use Database\Seeders\KategoriSeeder;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/',[WelcomeController::class,'index']);
 Route::get('/level',[LevelController::class,'index']);
 Route::get('/kategori',[KategoriController::class,'index']);
