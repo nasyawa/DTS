eb08b73dbc6e2e2a4af3fd7213013fb120fcdc18
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2024-08-19 23:01:40 +0700

    Handsout 24

---
+++ Pertemuan20/pwl-pos/app/Http/Controllers/UserController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LevelModel;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class UserController extends Controller
+{
+    //menampilkan hal user awal
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
+        $page = (object)[
+            'title' => 'Daftar user yg terdaftar dalam sistem'
+        ];
+        $activeMenu = 'user'; //set menu yg sedang akatif
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+    }
+    public function list(Request $request)
+    {
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
+            ->with('level');
+        return DataTables::of($users)
+            //nambah kolom index/no urut
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($user) { //nambah kolom aksi
+                $btn = '<a href="' . url('/user/' . $user->user_id) . '"class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' .
+                    url('/user/' . $user->user_id) . '">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" 
+            onclick="return confirm(\'Apakah Anda yakit menghapus data 
+            ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html 
+            ->make(true);
+    }
+    //menampilka hal form tambah user
+    public function create()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Tambah User Baru',
+            'list' => ['Home', 'User', 'Create']
+        ];
+        $level = LevelModel::all(); //ambil data level untuk ditampilkan di form
+        $activeMenu = 'user'; //set menu aktive
+
+        $page = (object)[
+            'title' => 'Daftar user yg terdaftar dalam sistem'
+        ];
+        return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+    }
+    //menyimapan data user baru
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:5', // Perbaikan: menambahkan tanda | untuk aturan `min`
+            'level_id' => 'required|integer' // Perbaikan: menambahkan tanda | untuk aturan `integer`
+        ]);
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password), // Mengenkripsi password sebelum disimpan
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user')->with('success', 'Data user berhasil disimpan');
+    }
+    // membuat fungdi show
+    public function show(string $id)
+    {
+        $user = UserModel::with('level')->find($id);
+
+        $breadcrumb = (object)[
+            'title ' => 'Detail User',
+            'list' => ['Home', 'User', 'Detail']
+        ];
+        $page = (object)[
+            'title' => 'Detail User'
+        ];
+        $activeMenu = 'user'; //set menu yg sedang aktif
+        return view('user.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'activeMenu' => $activeMenu]);
+    }
+    //menalmpilkan hal form edit user
+    public function edit(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::all();
+
+        $breadcrumb = (object)[
+            'title' => 'Edit User',
+            'list' => ['Home', 'User', "Edit"]
+        ];
+        $page = (object)[
+            'title' => 'Edit User'
+        ];
+        $activeMenu = 'user';
+        return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'level' => $level, 'activeMenu' => $activeMenu]);
+    }
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            // username harus diisi, berupa string, minimal 3 karakter,
+            // dan bernilai unik di tabel m_user kolom username kecuali untuk user dengan id yang sedang diedit 
+            'username' => 'required|string|min:3|unique:m_user,username,' . $id . ', user_id',
+            'nama' => 'required|string|max: 100', // nama harus diisi, berupa string, dan maksimal 100 karakter
+            'password' => 'nullable min:5',  // password bisa diisi (minimal 5 karakter) dan bisa tidak diisi
+            'level_id' => 'required integer'  // level_id harus diisi dan berupa angka
+        ]);
+        UserModel::find($id)->update([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => $request->password ? bcrypt($request->password) : UserModel::find($id)->password,
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user')->with('succes', ' Data user berhasil diubah');
+    }
+}
---
+++ Pertemuan20/pwl-pos/app/Models/UserModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class UserModel extends Model
+{
+    use HasFactory;
+    protected $primaryKey = "user_id";
+    protected $table = "m_user";
+    // protected $guarded = ["user_id"];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+
+    // public function level(): HasOne
+    // {
+    //     return $this->hasOne(LevelModel::class, "level_id");
+    // }
+    public function level() 
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+    }
+}
