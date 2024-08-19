97c8e51ccc447be60291eb8c36d2b816f5a83fdc
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2024-08-19 23:09:02 +0700

    revisi - Pt 24

--- Pertemuan20/pwl-pos/app/Http/Controllers/UserController.php
+++ Pertemuan20/pwl-pos/app/Http/Controllers/UserController.php
@@ -113,10 +113,10 @@ class UserController extends Controller
         $request->validate([
             // username harus diisi, berupa string, minimal 3 karakter,
             // dan bernilai unik di tabel m_user kolom username kecuali untuk user dengan id yang sedang diedit 
-            'username' => 'required|string|min:3|unique:m_user,username,' . $id . ', user_id',
+            'username' => 'required|string|min:3|unique:m_user,username,' . $id . ',user_id',
             'nama' => 'required|string|max: 100', // nama harus diisi, berupa string, dan maksimal 100 karakter
-            'password' => 'nullable min:5',  // password bisa diisi (minimal 5 karakter) dan bisa tidak diisi
-            'level_id' => 'required integer'  // level_id harus diisi dan berupa angka
+            'password' => 'nullable|min:5',  // password bisa diisi (minimal 5 karakter) dan bisa tidak diisi
+            'level_id' => 'required|integer'  // level_id harus diisi dan berupa angka
         ]);
         UserModel::find($id)->update([
             'username' => $request->username,
